af9d32570f1d4870b8bbee1d2f68f930f4ad1c27
Rajeshaalam <52394096+Rajeshaalam@users.noreply.github.com>
2020-07-30 19:47:17 +0530

    Add files via upload

---
+++ dayy3script.py
@@ -0,0 +1,86 @@
+#print(variables list)
+a,b = 25,26
+'''print(a,b)
+print(a,b,sep = ',')
+print(a,b,sep = ':')
+print(a,b,sep = '\n')
+print(a,b,sep = '\t')
+
+a=25;b=15
+print("hello everyone")
+print("good evening",end='\t')
+print("hey how are you",end = "\n")
+
+#print("string",variables list)
+print(a,b)
+print("You have entered",a,b,"values")
+print(a,b,"are the values you have entered")
+print("a=",a)
+
+#print(formatted string) statement
+
+#print("formatted string"%(variables list))
+
+#%d,%i --> int,%f-->float,%s-->string,%c-->char
+
+print("a = %d,b=%.1f"%(a,b))
+
+print("a=%d,b=%i"%(b,a))
+
+name = "Codegnan"
+
+print("Hello %s"%name)
+
+print("Hello %c"%name[0])
+
+print("Hello %20s"%name)
+
+#print('format string with replacement fields'.format(values))
+
+#{} -->relacement field
+
+a,b,c=15,25,26
+
+#print(a,b,c)
+
+#print("a=%d,b=%i,c=%d"%(a,b,c))
+
+print("number1 = {}".format(b))
+
+print("{1},{0},{2}".format(a,b,c))
+
+a ="codegnan";b ="vijayawada"
+
+print("{1} is in {0}".format(a,b))
+
+#sample input statements
+
+c = input()
+print(c)
+print(type(c))
+
+d = int(input("enter a number"))
+print(d)
+print(type(d))
+
+e = float(input("enter a number"))
+print(e)
+print(type(e))'''
+
+x= ["ab" , "cd"]
+
+for i in x:
+
+    i.upper()
+
+print(x)
+
+
+
+
+
+
+
+
+
+
